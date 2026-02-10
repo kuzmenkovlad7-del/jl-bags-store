42793a3dd93a8f3f9c5e5bb9aba9e958c721cf1b
Vlad Kuzmenko <a1111@MacBookPro.netis.cc>
2026-02-10 09:01:43 +0200

    fix(product): prevent false 404 on slug pages and force top scroll from catalog

--- app/[locale]/product/[slug]/page.tsx
+++ app/[locale]/product/[slug]/page.tsx
@@ -1,3 +1,6 @@
+export const dynamicParams = true
+export const revalidate = 0
+export const dynamic = 'force-dynamic'
 import { notFound } from 'next/navigation'
 import Image from 'next/image'
 import Link from 'next/link'
